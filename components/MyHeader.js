29fdc47374bb9399600ca1ed2b9eef1575524486
AayushiRockz <aayushisomkuwar993@gmail.com>
2021-03-02 22:57:52 +0530

    correction in icons and badges

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,11 +1,43 @@
 import React , {Component} from 'react';
-import {Header} from 'react-native-elements';
+import {Header, Icon , Badge} from 'react-native-elements';
 import {View,Text} from 'react-native';
-import {Icon} from 'react-native-elements';
+import db from '../config'
+import firebase from 'firebase';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 
 export default class MyHeader extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+          userId : firebase.auth().currentUser.email,
+          value:""
+        }
+      }
+    
+    getNumberOfUnreadNotifications(){
+      db.collection('all_notifications').where('notification_status','==',"unread").where('targeted_user_id','==',this.state.userId)
+      .onSnapshot((snapshot)=>{
+        var unreadNotifications = snapshot.docs.map((doc) => doc.data())
+        this.setState({
+          value: unreadNotifications.length
+        })
+      })
+    }
+
+    
+ BellIconWithBadge=()=>{
+    return(
+      <View>
+        <Icon name='bell' type='font-awesome' color='#ffffff' size={25}
+          onPress={() =>this.props.navigation.navigate('Notification')}/>
+         <Badge
+          value={this.state.value}
+         containerStyle={{ position: 'absolute', top: -4, right: -4 }}/>
+      </View>
+    )
+  }
+
     render(){
         return(
             <SafeAreaProvider>
@@ -23,6 +55,7 @@ export default class MyHeader extends Component{
            padding:20} }}
             backgroundColor = "#FAC1CC"
             leftComponent={<Icon name='bars' type='font-awesome' color='#ffffff'  onPress={() => this.props.navigation.toggleDrawer()}/>}
+            rightComponent={<this.BellIconWithBadge {...this.props}/>}
             
             />
 
